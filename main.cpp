774db6d7014277ff78fc9cfd9d2ab4d0db82f3b3
ParthSSharma <parthssharma@outlook.com>
2023-07-25 15:09:16 -0400

    Added comment to explain what I did

--- main.cpp
+++ main.cpp
@@ -1,13 +1,13 @@
-#include "pico/stdlib.h"
-#include "pico/time.h"
-#include "hardware/adc.h"
-#include "hardware/gpio.h"
 #include <stdint.h>
 #include <stdio.h>
+#include "Bitmap.h"
+#include "pico/time.h"
+#include "pico/stdlib.h"
+#include "hardware/i2c.h"
+#include "hardware/adc.h"
+#include "hardware/gpio.h"
 #include "pico-ssd1306/ssd1306.h"
 #include "pico-ssd1306/textRenderer/TextRenderer.h"
-#include "hardware/i2c.h"
-#include "Bitmap.h"
 
 #define I2C_PORT        i2c0
 #define I2C_SCL         28U
@@ -28,6 +28,13 @@ void RunBadApple(pico_ssd1306::SSD1306 display){
         u64StartTime = time_us_64();
         for(uint32_t row = 0; row < NUM_ROWS; row++){
             for(uint32_t col = 0; col < NUM_COLS; col++){
+                /*
+                    Each element of BadAppleBitmap 2D array is a uint16_t integer. The rows of the array give information related to each frame.
+                    Each bit in an element represents a pixel of the screen. If the bit is 0, the pixel is black and if the bit is 1, the pixel
+                    is white. Therefore, each element gives us information related to 16 pixels. With some bit shifts and a mask to test the bit,
+                    we can decode the pixel value. In the function "display.setPixel", the last parameter is 0 for set and 1 for clear. Therefore,
+                    we have to logically invert the pixel.
+                */
                 display.setPixel(col + COL_START, row + ROW_START, (pico_ssd1306::WriteMode) !(BadAppleBitmap[frame][(row * (NUM_COLS / PIXELS_PER_ELEMENT)) + (col / PIXELS_PER_ELEMENT)] & (1 << ((PIXELS_PER_ELEMENT - 1) - (col % PIXELS_PER_ELEMENT)))));
             }
         }
